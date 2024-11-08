7f6755e339b9af6a9af4173411d2371821d9b9da
ngdikman <diwen.0906@163.com>
2024-11-08 11:50:11 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Geek',
-  subtitle: 'A Geeker Site',
+  title: 'BACKIN',
+  subtitle: 'A Site For Geeker',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 265,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
